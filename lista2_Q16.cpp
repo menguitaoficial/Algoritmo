7d09d193bb2ed7c9008de4e11d4e1b3bc922d6b4
Caio Diogenes Almeida Jácome Alves <134067995+menguitaoficial@users.noreply.github.com>
2025-04-29 11:01:38 -0300

    Update lista2_Q16.cpp

--- lista2_Q16.cpp
+++ lista2_Q16.cpp
@@ -1,6 +1,19 @@
+/*16. Leia a idade de uma pessoa e escreva se ela já tem idade para tirar carteira de habilitação (18 anos 
+completos) ou não. Se não tiver idade, escreva também quantos anos faltam para que possa tirar a 
+carteira. */
+
 #include <iostream>
 using namespace std;
 
 int main (){
-	int idade;
-}
+    int idade;
+    cout <<"Digite a idade de uma pessoa:";
+    cin >> idade;
+    if (idade >= 18){
+        cout <<"tem idade de tirar carteira de habilitação";
+    }else{
+      int anosfaltando = 18 - idade;
+      cout <<"não pode tirar a carteira de habilitação";
+      cout <<"faltam" << anosfaltando << "quantos anos faltam para carteira";
+    }
+ }
